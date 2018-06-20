924da8d98e2669beb6937f0bdc2858412a88ae9b
Fernando <fernando.zmorales@gmail.com>
2018-06-20 09:37:39 -0500

    Quitando user

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@
 from app import db
 
 class User(db.Model):
-    __tablename__ = 'user'
+    __tablename__ = 't_user'
     id = db.Column(db.Integer, primary_key=True)
     public_id = db.Column(db.String(50), unique=True)
     display_name = db.Column(db.String(50))
@@ -12,4 +12,3 @@ class User(db.Model):
     role = db.Column(db.Integer)
     created_on = db.Column(db.DateTime, server_default=db.func.now())
     updated_on = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
-    my_business = db.relationship('Business', secondary=user_business, backref=db.backref('partners', lazy='dynamic'))
